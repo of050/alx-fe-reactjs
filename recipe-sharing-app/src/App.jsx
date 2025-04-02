f96f3defbf90ed9510233b7ee3a9efcdf50c5b20
of050 <of05625@gmail.com>
2025-04-02 21:23:20 +0200

    Updating Files

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeDetails from './components/RecipeDetails';
 
 const App = () => {
   return (
