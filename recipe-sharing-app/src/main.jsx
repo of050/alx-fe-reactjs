98af912d2df523cbe5d59f8089f8ef60df0d1148
of050 <of05625@gmail.com>
2025-04-02 21:09:17 +0200

    Updating Files

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import RecipeDetails from './components/RecipeDetails';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+    </Routes>
+  </Router>
+);
