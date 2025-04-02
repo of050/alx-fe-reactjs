6465d7f5382f24b98521b21b9723ad5c629ac15a
of050 <of05625@gmail.com>
2025-04-02 21:07:55 +0200

    Updating Files

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,6 +1,7 @@
 // src/components/DeleteRecipeButton.jsx
 import React from 'react';
 import useRecipeStore from '../recipeStore';
+import { useNavigate } from 'react-router-dom';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
