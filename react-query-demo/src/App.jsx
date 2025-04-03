33bd7170c04b5aa7be1d65428278dae256da48f2
of050 <of05625@gmail.com>
2025-04-03 14:35:03 +0200

    Initial commit

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,13 +1,21 @@
 // src/App.jsx
 import React from 'react';
-import PostsComponent from './PostsComponent';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import PostsComponent from './PostsComponent'; // Make sure to import your PostsComponent
 
-const App = () => {
+// Create a QueryClient instance
+const queryClient = new QueryClient();
+
+function App() {
   return (
-    <div>
-      <PostsComponent />
-    </div>
+    // Wrap your application with QueryClientProvider
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>My React Query App</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
-};
+}
 
 export default App;
