b8cff41e17a0157c409637d8b142f7d2f6e8c3f6
of050 <of05625@gmail.com>
2025-04-03 14:09:03 +0200

    Initial commit

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,76 +1,42 @@
 // src/components/RegistrationForm.js
 import React, { useState } from 'react';
-
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 const RegistrationForm = () => {
-    const [formData, setFormData] = useState({
-        username: '',
-        email: '',
-        password: ''
-    });
-
-    const [errors, setErrors] = useState({});
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
-
-    const validate = () => {
-        const newErrors = {};
-        if (!formData.username) newErrors.username = 'Username is required';
-        if (!formData.email) newErrors.email = 'Email is required';
-        if (!formData.password) newErrors.password = 'Password is required';
-        return newErrors;
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const validationErrors = validate();
-        if (Object.keys(validationErrors).length === 0) {
-            console.log('Form submitted:', formData);
-            // Here you would typically send the data to your API
-        } else {
-            setErrors(validationErrors);
-        }
-    };
-
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Username:</label>
-                <input
-                    type="text"
-                    name="username"
-                    value={formData.username}
-                    onChange={handleChange}
-                />
-                {errors.username && <p>{errors.username}</p>}
-            </div>
-            <div>
-                <label>Email:</label>
-                <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                />
-                {errors.email && <p>{errors.email}</p>}
-            </div>
-            <div>
-                <label>Password:</label>
-                <input
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                />
-                {errors.password && <p>{errors.password}</p>}
-            </div>
-            <button type="submit">Register</button>
-        </form>
+        <Formik
+            initialValues={{
+                username: '',
+                email: '',
+                password: '',
+            }}
+            validationSchema={validationSchema}
+            onSubmit={(values) => {
+                console.log('Form submitted:', values);
+                // Send the data to your API here
+            }}
+        >
+            {() => (
+                <Form>
+                    <div>
+                        <label>Username:</label>
+                        <Field name="username" />
+                        <ErrorMessage name="username" component="p" />
+                    </div>
+                    <div>
+                        <label>Email:</label>
+                        <Field name="email" type="email" />
+                        <ErrorMessage name="email" component="p" />
+                    </div>
+                    <div>
+                        <label>Password:</label>
+                        <Field name="password" type="password" />
+                        <ErrorMessage name="password" component="p" />
+                    </div>
+                    <button type="submit">Register</button>
+                </Form>
+            )}
+        </Formik>
     );
 };
 
