3471be1a377e4a2f949c37af764344c98780ad8b
of050 <of05625@gmail.com>
2025-04-03 14:25:36 +0200

    Initial commit

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,50 +1,19 @@
-// src/components/RegistrationForm.js
 import React, { useState } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+
 const RegistrationForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle registration logic here
-    console.log("Email:", email, "Password:", password);
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email} // Controlled input
-          onChange={handleEmailChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password} // Controlled input
-          onChange={handlePasswordChange}
-          required
-        />
-      </div>
-      <button type="submit">Register</button>
-    </form>
-  );
+    const [username, setUsername] = useState('');
+    return (
+        <form>
+            <label htmlFor="username">Username:</label>
+            <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            {/* Other form elements... */}
+        </form>
+    );
+    // Other states and functions...
 };
-
 export default RegistrationForm;
