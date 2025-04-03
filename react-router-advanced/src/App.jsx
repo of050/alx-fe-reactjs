655fe65b25091030f570c65156854e69d8e4176b
of050 <of05625@gmail.com>
2025-04-03 14:47:59 +0200

    Initial commit

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import NotFound from './components/NotFound';
@@ -8,21 +8,23 @@ import BlogPost from './components/BlogPost';
 
 function App() {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/blog/1">Blog Post 1</Link>
-        <Link to="/blog/2">Blog Post 2</Link>
-        <Link to="/blog/3">Blog Post 3</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
-        <Route path="/blog/:id" element={<BlogPost />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/blog/1">Blog Post 1</Link>
+          <Link to="/blog/2">Blog Post 2</Link>
+          <Link to="/blog/3">Blog Post 3</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/*" element={<Profile />} />
+          <Route path="/blog/:id" element={<BlogPost />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
