2ecf028c498d83cebb3e10526543202a7f2235b7
of050 <of05625@gmail.com>
2025-04-03 13:38:14 +0200

    Initial commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const UserProfile = () => {
     return (
         <div className="md:p-8 sm:p-4 md:max-w-xs md:max-w-sm">
-            <img src="profile.jpg" alt="Profile" className="sm:w-24 sm:h-24 wd:w-36 wd:h-36 rounded-full" />
+            <img src="profile.jpg" alt="Profile" className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full" />
             <h1 className="sm:text-lg md:text-xl mt-4">User Profile</h1>
             <p className="text-sm sm:text-base mt-2">This is a user profile description.</p>
         </div>
