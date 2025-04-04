1f0997d2b293e7405d01c94019eead25b662177a
of050 <of05625@gmail.com>
2025-04-04 13:06:13 +0200

    Initial commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,15 +1,27 @@
+// src/services/githubService.js
 import axios from 'axios';
 
-export const searchUsers = async (username, location, repos, page = 1) => {
-  let query = '';
-  if (username) query += `${username} in:login`;
-  if (location) query += ` location:${location}`;
-  if (repos) query += ` repos:>=${repos}`;
+export const fetchUserData = async ({ username, location, minRepos }) => {
+  try {
+    // Construct query string based on provided filters
+    let query = '';
 
-  const url = `https://api.github.com/search/users?q=${encodeURIComponent(
-    query
-  )}&page=${page}&per_page=30`;
+    if (username) {
+      query += `${username} in:login`;
+    }
 
-  const response = await axios.get(url);
-  return response.data;
+    if (location) {
+      query += ` location:${location}`;
+    }
+
+    if (minRepos) {
+      query += ` repos:>=${minRepos}`;
+    }
+
+    const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+
+    return response.data.items; // returns an array of user objects
+  } catch (error) {
+    throw new Error('GitHub API request failed');
+  }
 };
